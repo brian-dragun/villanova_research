cd566cac8029368bca0180064653aa2b8ec9c388
Brian Dragun <bdragun@villanova.edu>
2025-03-13 00:42:48 +0000

    Update

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 # config.py
-MODEL_NAME = "meta-llama/Llama-2-7b-hf"  # Change this to any model name you wish to use.
-#MODEL_NAME = "gpt2"
+# MODEL_NAME = "meta-llama/Llama-2-7b-hf"  # Change this to any model name you wish to use.
+MODEL_NAME = "gpt2"
