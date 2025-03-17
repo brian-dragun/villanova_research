72991b09b75b4ea319076bd5103ec81a37d72276
Brian Dragun <bdragun@villanova.edu>
2025-03-17 15:49:51 +0000

    Updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install pybind11-dev
+sudo apt-get install libopenmpi-dev
+
+
 # Configure Git settings
 echo "Configuring Git..."
 git config --global credential.helper store
@@ -9,11 +15,12 @@ git config --global user.name "Brian Dragun"
 # Install Python dependencies
 echo "Installing requirements..."
 pip install -r requirements.txt
-
-pip install pybind11
-
 pip uninstall numpy
 pip install numpy<2.0
+
+# Set CPLUS_INCLUDE_PATH for pybind11 (optional)
+echo "Setting CPLUS_INCLUDE_PATH for pybind11..."
+export CPLUS_INCLUDE_PATH=$(python -m pybind11 --includes | sed 's/-I//g' | tr ' ' ':')
 
 # HuggingFace Login
 echo "Logging into Hugging Face..."
